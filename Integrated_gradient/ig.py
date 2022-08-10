1c3395d933151ed82e0f32c3dafcc572430bcb74
ttjh1234 <ttjh1234@naver.com>
2022-08-10 16:07:54 +0900

    XAI 0810

---
+++ Integrated_gradient/ig.py
@@ -0,0 +1,16 @@
+'''
+XAI Implement
+
+Axiomatic Attribution for Deep Networks
+
+Choi Sung Su
+
+First. Baseline Visualization
+
+Second. Why prior Gradients Attribution Method doesn't Sensitivity
+
+Thrid. Integrated Gradients Method Implement at Image Recognition Task  
+
+'''
+
+
