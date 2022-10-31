ef248c701272bbf3fcdef622f9b75b3c373e1b65
ttjh1234 <ttjh1234@naver.com>
2022-10-31 17:29:01 +0900

    Plan to Implement

--- Discretized_Integrated_gradient/implement/dig.py
+++ Discretized_Integrated_gradient/implement/dig.py
@@ -1,1 +1,13 @@
 # Plan to Implement
+
+# first anchor search algorithm
+
+def anchor_search():
+    anchor=[]
+    return anchor
+
+# second monotonize algorithm
+
+def monotonize_anchor(baseline,y,anchor):
+    anchor=[]
+    return anchor
