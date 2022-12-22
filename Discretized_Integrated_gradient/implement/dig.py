3c5844d8349e4c9b20ccb3ed30a1a687dd75f29e
ttjh1234 <ttjh1234@naver.com>
2022-12-22 22:39:43 +0900

    DIG Implement ing

--- Discretized_Integrated_gradient/implement/dig.py
+++ Discretized_Integrated_gradient/implement/dig.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(
 
 import torchtext
 from torchtext.datasets import IMDB
-
+from tqdm import tqdm
 from NLP.transformers_module import *
 
 train_iter,valid_iter,text_iter,INPUT_DIM,TEXT_PAD_IDX, TEXT=imdb_pytorch_load()
@@ -37,47 +37,10 @@ modeldir=os.getcwd().split("XAI")[0]
 model=sentiment_classification(enc,HID_DIM,src_pad_idx=TEXT_PAD_IDX,device=device).to(device)
 model.load_state_dict(torch.load(modeldir+'NLP/model/transformer-sentiment.pt'))
 
-print(model)
-
-'''
-TEXT.vocab.itos
-
-cand=torch.arange(0,10002,step=1).to(device)
-re=model.encoder.tok_embedding(cand)
-one=torch.ones(1)
-x=torch.LongTensor([1,3,4,6,7,8])
 
 
 
-a=model.encoder.tok_embedding(x.to(device))
-'''
-
-
-# first anchor search algorithm
-
-def word_approximation_error(w,word_embedding):
-    value=0
-    return value
-
-'''
-TEXT.vocab.stoi
-
-tok_embedding_layer=model.encoder.tok_embedding
-x=torch.LongTensor([1,3,4,6,7,8])
-'''
-
-for i in train_iter:
-    i
-    break
-
-def get_embedding_index(tok_embedding_layer,x):
-    results = torch.where(torch.sum((tok_embedding_layer.weight==x), axis=1))
-    if len(results[0])==len(x):
-        return None
-    else:
-        return results[0][0]
-
-
+# KNN w.r.t Words Embeddings
 
 def k_nearest_neighbor(tok_embedding_layer,x,k=10,flag=0):
     
@@ -98,7 +61,7 @@ def k_nearest_neighbor(tok_embedding_layer,x,k=10,flag=0):
     # K Neighbor words index of Sentence Word Embeddings.
     
     # This operation is very heavy. How can I ease this op? -> scikit learn - Use kneighbors_graph  
-    
+
     # words_neighbor=torch.argsort(torch.sum((emb_words-emb_cand)**2,dim=2))[:,1:(k+1)]
     
     distance_mat=kneighbors_graph(emb_cand,10,mode='distance',p=2)
@@ -121,30 +84,65 @@ def k_nearest_neighbor(tok_embedding_layer,x,k=10,flag=0):
     return emb_words,baseline_embed,words_neighbor,words_neighbor_embedding
 
 
+# monotonize algorithm
 
-def anchor_greedy(x,baseline,neighbor,iter):
+def monotonize_anchor(x,baseline,anchor,perturb_value):
+    
+    if anchor.dim()==3:
+        anchor=torch.transpose(anchor,1,0)
+        # K, Sentence length, Embedding dim
+    else:
+        anchor=anchor.unsqueeze(0)
+
+    upper_state=torch.where((baseline<=anchor)&(x>=anchor),1,-1)
+    lower_state=torch.where((baseline>=anchor)&(x<=anchor),1,-1)
+    
+    perturb_embed=torch.where((upper_state==1)|(lower_state==1),anchor,perturb_value)
+
+    words=perturb_embed.to(device)
+
+    
+    return words
+
+
+
+# anchor search algorithm
+
+def anchor_greedy(x,baseline,neighbor,perturb_value):
     
     # First, Calculate monotonize_embed of KNN(w).
+    monotone_cand=monotonize_anchor(x,baseline,neighbor,perturb_value).transpose(1,0)
+    anchor_index=torch.argmin(torch.sum((neighbor-monotone_cand)**2,dim=2),dim=1)
     
-    monotone_cand=monotonize_anchor(x,baseline,neighbor,iter)
-    anchor_index=torch.argmin(torch.sum((neighbor-monotone_cand)**2,dim=1))
+    anchor_index=anchor_index.unsqueeze(1)
+    nwords=torch.arange(0,500,step=1).unsqueeze(1)
     
-    anchor=monotone_cand[anchor_index]
-    next_words_embed=neighbor[anchor_index]
+    index_tensor=torch.cat((nwords,anchor_index),dim=1)
+        
+    # 500,10,256 Tensor -> 500,256 Tensor 
+    
+    anchor=monotone_cand[index_tensor[:,0],index_tensor[:,1],:]
+    next_words_embed=neighbor[index_tensor[:,0],index_tensor[:,1],:].unsqueeze(0)
     
     return anchor,next_words_embed
 
 
 def anchor_max_count(x,baseline,neighbor):
     
+    neighbor=torch.transpose(neighbor,1,0)
+    
     upper_state=torch.where((baseline<=neighbor)&(x>=neighbor),1,-1)
     lower_state=torch.where((baseline>=neighbor)&(x<=neighbor),1,-1)
     
     n_state=torch.where((upper_state==1)|(lower_state==1),1,0)
+
+    highest_num_index=torch.argmax(torch.sum(n_state,dim=2),dim=0).unsqueeze(1)
+    nwords=torch.arange(0,500,step=1).unsqueeze(1)
+    index_tensor=torch.cat((nwords,highest_num_index),dim=1)
     
-    highest_num_index=torch.argmax(torch.sum(n_state,dim=2))
-    
-    anchor=neighbor[highest_num_index]
+    neighbor=torch.transpose(neighbor,1,0)
+        
+    anchor=neighbor[index_tensor[:,0],index_tensor[:,1],:]
 
     return anchor
 
@@ -155,125 +153,32 @@ def anchor_search(tok_embedding_layer, x, m=30,method='greedy'):
     # Calculate neighbor of x.
     input_emb, baseline_emb, _, neighbor=k_nearest_neighbor(tok_embedding_layer, x, k=10,flag=1)
     
+    # Calculate Perturb Value
+    perturb_value=input_emb-(input_emb-baseline_emb)/m
+    
     # Greedy Heuristic Algorithm.
     if method=='greedy':
-        for i in range(m):
-            anchor_temp,next_words_embed=anchor_greedy(input_emb, baseline_emb, neighbor,m)
-            _, _, _, neighbor=k_nearest_neighbor(tok_embedding_layer, next_words_embed, k=10)
-            words[i]=anchor_temp
+        for _ in tqdm(range(m)):
+            anchor_temp,next_words_embed=anchor_greedy(input_emb, baseline_emb, neighbor, perturb_value)
+            input_emb, _, _, neighbor=k_nearest_neighbor(tok_embedding_layer, next_words_embed, k=10)
+            anchor_temp=anchor_temp.unsqueeze(0)
+            words=torch.cat((words,anchor_temp),dim=0)
+
     
     # MaxCount Algorithm.
     else:
-        anchor=anchor_max_count(input_emb, baseline_emb, neighbor)
-        anchor_temp=monotonize_anchor(x,baseline_emb,anchor,m)
-        _, _, _, neighbor=k_nearest_neighbor(tok_embedding_layer, anchor, k=10)
-        words[i]=anchor_temp
-    
+        for _ in tqdm(range(m)):
+            anchor=anchor_max_count(input_emb, baseline_emb, neighbor)
+            anchor_temp=monotonize_anchor(input_emb, baseline_emb, anchor, perturb_value)
+            input_emb, _, _, neighbor=k_nearest_neighbor(tok_embedding_layer, anchor, k=10)
+            
+            words=torch.cat((words,anchor_temp),dim=0)
+
     return words
 
-text_data=i.text[[0]]
-
-text_data.shape
-
-device='cpu'
-model=model.to(device)
-
-d=anchor_search(model.encoder.tok_embedding, text_data, m=30,method='greedy')
-words=torch.zeros(0,500,256).to(device)
-
-tok_embedding_layer=model.encoder.tok_embedding
-x=text_data
-# Calculate neighbor of x.
-input_emb, baseline_emb, _, neighbor=k_nearest_neighbor(tok_embedding_layer, x, k=10,flag=1)
-
-input_emb.shape
-
-baseline_emb.shape
-
-t1,t2=anchor_greedy(input_emb,baseline_emb,neighbor,30)
-
-t1.shape
-
-t2.shape
-
-x=input_emb
-baseline=baseline_emb
-neighbor=neighbor
-iter=30
-
-# Reimplementaion
-def anchor_greedy(x,baseline,neighbor,iter):
-    
-    # First, Calculate monotonize_embed of KNN(w).
-    monotone_cand=monotonize_anchor(x,baseline,neighbor,iter)
-    anchor_index=torch.argmin(torch.sum((neighbor-monotone_cand)**2,dim=2),dim=1)
-    
-    anchor_index=anchor_index.unsqueeze(1)
-    monotone_cand.shape
-    anchor_index.shape
-    
-    anchor_index
-    monotone_cand.shape
-    
-    monotone_cand[anchor_index,:].shape
-    
-    # 500,10,256 Tensor -> 500,256 Tensor 
-    
-    torch.transpose(monotone_cand,1,0).shape
-    torch.transpose(monotone_cand,1,0)[anchor_index].shape
-    torch.gather(monotone_cand,2,index=anchor_index).shape
-    torch.gather(torch.transpose(monotone_cand,1,0),1,index=anchor_index)
-    
-    
-    
-    anchor=monotone_cand[anchor_index]
-    next_words_embed=neighbor[anchor_index]
-    anchor.shape
-    return anchor,next_words_embed
 
 
-# second monotonize algorithm
-
-'''
-TEXT.vocab.stoi[1]
-
-# 1 is pad 38,39,41 test
-zero_pad=emb_cand[[1]]
-test=emb_cand[[38,39,41]]
-zero_pad.shape
-test.shape
-t1.shape
-t1=test[:1]
-test[1:].shape
-state_tensor=torch.where((zero_pad<=test[1:])&(t1>=test[1:]),1,-1)
-
-torch.where()
-
-state_tensor.shape
-
-x=test[:1]
-baseline=zero_pad
-baseline.shape
-anchor=test[1:]
-
-anchor.shape
-'''
-
-def monotonize_anchor(x,baseline,anchor,iter):
-    
-    perturb_value=x-(x-baseline)/iter
-    anchor=torch.transpose(anchor,1,0)
-
-    upper_state=torch.where((baseline<=anchor)&(x>=anchor),1,-1)
-    lower_state=torch.where((baseline>=anchor)&(x<=anchor),1,-1)
-    
-    perturb_embed=torch.where((upper_state==1)|(lower_state==1),anchor,perturb_value)
-
-    words=perturb_embed.to(device)
-
-    
-    return torch.transpose(words,1,0)
-
+# Discretized Integrated Gradients
 
 def discretized_integrated_gradients(x,baseline,model,method='greedy'):
     
@@ -293,6 +198,12 @@ def discretized_integrated_gradients(x,baseline,model,method='greedy'):
     
     return None
 
+
+# Metrics
+
+def word_approximation_error(w,word_embedding):
+    value=0
+    return value
 
 def log_odds(x,model,k):
     
@@ -388,7 +299,61 @@ def sufficiency(x,model):
     # suff=torch.mean(perturb_prob-predict_prob)
     
     suff=0
-    return suff    
+    return suff
+
+
+
+'''
+# Test
+for i in train_iter:
+    i
+    break
+
+text_data=i.text[[0]]
+
+text_data.shape
+
+device='cpu'
+model=model.to(device)
+
+d=anchor_search(model.encoder.tok_embedding, text_data, m=30,method='greedy')
+
+d2=anchor_search(model.encoder.tok_embedding, text_data, m=30,method='max_count')
+
+words=torch.zeros(0,500,256).to(device)
+
+tok_embedding_layer=model.encoder.tok_embedding
+x=text_data
+# Calculate neighbor of x.
+input_emb, baseline_emb, _, neighbor=k_nearest_neighbor(tok_embedding_layer, x, k=10,flag=1)
+
+input_emb.shape
+
+baseline_emb.shape
+
+t1,t2=anchor_greedy(input_emb,baseline_emb,neighbor,30)
+
+t1.shape
+
+t2.shape
+
+x=input_emb
+baseline=baseline_emb
+neighbor=neighbor
+iter=30
+
+
+tdb=d[:,0,:]
+
+tdb.shape
+
+tdb[1]<=tdb[2]
+tdb[2]<=tdb[3]
+
+words=torch.zeros(0,500,256).to(device)
+
+'''
+
 
 
 
