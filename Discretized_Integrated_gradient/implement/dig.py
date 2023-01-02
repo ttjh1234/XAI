d12938b36effc1fab4eb360eb7f7fbd6b66cdbc2
ttjh1234 <ttjh1234@naver.com>
2023-01-02 21:15:24 +0900

    Implement ing

--- Discretized_Integrated_gradient/implement/dig.py
+++ Discretized_Integrated_gradient/implement/dig.py
@@ -19,7 +19,7 @@ from konlpy.tag import Okt
 from torch.utils.data import TensorDataset, DataLoader,Dataset, random_split
 import json
 from tqdm import tqdm
-
+from XAI.Integrated_gradient.implement.ig_pytorch import *
 
 train_iter,valid_iter,text_iter,INPUT_DIM,TEXT_PAD_IDX, TEXT=imdb_pytorch_load()
 
@@ -27,8 +27,8 @@ train_iter,valid_iter,text_iter,INPUT_DIM,TEXT_PAD_IDX, TEXT=imdb_pytorch_load()
 word2index=TEXT.vocab.stoi
 index2word=TEXT.vocab.itos
 
-HID_DIM = 256
-ENC_LAYERS = 3
+HID_DIM = 128
+ENC_LAYERS = 4
 ENC_HEADS = 8
 ENC_PF_DIM = 512
 ENC_DROPOUT = 0.1
@@ -42,186 +42,66 @@ enc = Encoder(INPUT_DIM,
               device)
 
 modeldir=os.getcwd().split("XAI")[0]
-
 model=sentiment_classification(enc,HID_DIM,src_pad_idx=TEXT_PAD_IDX,device=device).to(device)
-model.load_state_dict(torch.load(modeldir+'NLP/model/transformer-sentiment.pt'))
 
-# Plan to implement
-
-# Discretized Integrated Gradients
-
-def discretized_integrated_gradients(x,baseline,model,method='greedy'):
-     
-    return None
-
-# Test Code
-
-# test code
-for i in train_iter:
-    i
-    break
-i
-text_data=i.text[3]
-text_label=i.label[3]
+def initialize_weights(m):
+    if hasattr(m, 'weight') and m.weight.dim() > 1:
+        nn.init.xavier_uniform_(m.weight.data)
 
 
-sentence=[]
-for w_index in text_data:
-    if index2word[w_index]!='<pad>':
-        sentence.append(index2word[w_index])
-        
-print(' '.join(sentence))
+model.apply(initialize_weights)
 
-device='cpu'
-model=model.to(device)
+optimizer = torch.optim.Adam(model.parameters())
+criterion = nn.BCELoss()  
 
-tok_embedding_layer=model.encoder.tok_embedding
+N_EPOCHS = 100
+CLIP = 1
+patient=0
 
-x=text_data
+best_valid_loss = float('inf')
 
-adj=create_knn_matrix(tok_embedding_layer)
-word_features=tok_embedding_layer.weight.cpu().detach().numpy()
-word_idx_map=word2index
-
-
-
-baseline=torch.ones(500).type(torch.int)
-
-path=making_interpolation_path(x, baseline, 'cuda', (word_idx_map, word_features, adj), steps=30, strategy='greedy')
-
-model=model.to('cuda')
-
-model.eval()
-#model.encoder.
-model.zero_grad()
-
-output=model(path).sum()
-
-path=path.to('cuda')
-path.requires_grad=True
-output.shape
-
-output.backward()
-
-print(path.grad)
-
-path_grad=path.grad
-path=path.cpu().detach()
-path_grad=path_grad.cpu().detach()
-
-
-
-
-
-path_grad.shape
-
-path.shape
-tuple(path)[0][1:].shape
-
-
-
-shifted_inputs_tpl	= tuple(torch.cat([scaled_features[1:], scaled_features[-1].unsqueeze(0)]) for scaled_features in tuple(path))
-steps				= tuple(shifted_inputs_tpl[i] - tuple(path)[i] for i in range(len(shifted_inputs_tpl)))
-scaled_grads		= tuple(path_grad[i] * steps[i] for i in range(path_grad.shape[0]))
-
-
-for i in scaled_grads:
-    print(i.shape)
-
-
-att=[]
-for i in scaled_grads:
-    att.append(torch.sum(i,dim=1))
-
-np.array(att).shape
+for epoch in range(N_EPOCHS):
     
-np.array(att)
-
-
-
-np.array(att).shape
-
-scaled_grads[0].shape
-
-path.shape
-
-multiply=torch.zeros(0,500,256).to('cuda')
-
-baseline_emb=tok_embedding_layer.weight[1]
-
-baseline_emb=baseline_emb.to('cpu')
-
-dig_vec=(path-baseline_emb/32)*path_grad
-
-dig_vec=dig_vec.reshape(500,32,256)
-
-dig=torch.sum(dig_vec,dim=1)
-
-final_dig=torch.sum(dig,dim=1)
-
-final_dig
-
-att=final_dig.detach().numpy()
-
-multiply.shape
-
-path.shape[0]
-
-def get_color(attr):
-    if attr > 0:
-        g = int(128*attr) + 127
-        b = 128 - int(64*attr)
-        r = 128 - int(64*attr)
+    start_time = time.time()
+    
+    train_loss = train(model, train_iter, optimizer, criterion, CLIP)
+    valid_loss = evaluate(model, valid_iter, criterion)
+    
+    end_time = time.time()
+    
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+    print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. PPL: {math.exp(valid_loss):7.3f}')
+    if valid_loss < best_valid_loss:
+        best_epoch=epoch
+        print('Best Epoch : ',best_epoch)
+        
+        patient=0
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), './assets/transformer_sentiment.pt')
     else:
-        g = 128 + int(64*attr)
-        b = 128 + int(64*attr)
-        r = int(-128*attr) + 127
-    return r,g,b
-
-final_dig.detach().numpy()
-np.min(final_dig.detach().numpy())
-
-att=final_dig/torch.norm(final_dig)
-att=att.to('cpu').detach().numpy()
-scaled_att = (att - np.min(att)) / (np.max(att)- np.min(att)) * 2 - 1
-
-cmap_bound=np.abs(att).max()
-
-att2=att/cmap_bound
-
-att2=(att.max()-att)/(att.max()-att.min())
-
-att3=att2*2-1
-
-rgblist=[]
-
-for i in scaled_att:
-    rgblist.append(get_color(i))
-
-str=""
-for i,c in zip(sentence,rgblist):
-    if i!="<pad>":
-        str=str+"\033[38;2;{};{};{}m {} \033[0m".format(c[0],c[1],c[2],i)
-        
-print(str)
-
-def colorize(attrs,cmap='PiYG'):
-    cmap_bound=np.abs(attrs).max()
-    norm=mpl.colors.Normalize(vmin=-cmap_bound,vmax=cmap_bound)
-    cmap=mpl.cm.get_cmap(cmap)
+        print('Best Epoch : ',best_epoch)
+        patient+=1
+        if patient>15:
+            break
     
-    colors=list(map(lambda x:mpl.colors.to_rgb(cmap(norm(x))),attrs))
-    return colors
+model.load_state_dict(torch.load('./assets/transformer-sentiment.pt'))
+test_loss = evaluate(model, text_iter, criterion)
+print(f'| Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):7.3f} |')
+accuracy_score(model,train_iter,valid_iter,text_iter)
 
-att=final_dig.detach().numpy()
 
 
-colors=colorize(att)
 
-list(map(lambda x : mpl.colors.hex2color(x),colors))
-colors[0]*255
 
-color_rgb = tuple([int(c*255) for c in colors])
+
+
+
+
+
+
+
 
 
 ### 한국어로 모델 재학습 및 test.
@@ -257,7 +137,6 @@ class CustomDataset(Dataset):
 
 dataset = CustomDataset()
 
-
 datasetsize= len(dataset)
 train_size = int(datasetsize * 0.8)
 valid_size = int(datasetsize * 0.1)
@@ -271,9 +150,6 @@ print(f"Testing Data Size : {len(test_dataset)}")
 train_iter = DataLoader(train_dataset, batch_size=64, shuffle=True, drop_last=False)
 valid_iter = DataLoader(valid_dataset, batch_size=64, shuffle=True, drop_last=False)
 test_iter = DataLoader(test_dataset, batch_size=64, shuffle=True, drop_last=False)
-
-for i in train_iter:
-    break
 
 INPUT_DIM=dataset.vocab_size
 
@@ -342,38 +218,39 @@ accuracy_score_kor(model,train_iter,valid_iter,test_iter)
 
 # 비교군 : simple GRU Model 
 
-class simpleGRN(nn.Module):
+class simpleGRU(nn.Module):
     def __init__(self,input_dim,emb_hidden,hidden_size,num_layers,num_classes=1):
         super().__init__()
         self.num_layers=num_layers
         self.embedding=nn.Embedding(input_dim,emb_hidden)
         self.gru=nn.GRU(emb_hidden,hidden_size,num_layers,batch_first=True)
-        self.fc1=nn.Linear(hidden_size,hidden_size)
-        self.fc2=nn.Linear(hidden_size,num_classes)
-        self.activation1=nn.ReLU()
+        #self.fc1=nn.Linear(hidden_size,hidden_size)
+        self.fc1=nn.Linear(hidden_size,num_classes)
+        #self.fc2=nn.Linear(hidden_size,num_classes)
+        #self.activation1=nn.ReLU()
         self.activation2 = nn.Sigmoid()
         
     def forward(self, input):
-        out=self.embedding(input)
+        if input.dim()==2:
+            out=self.embedding(input)
+        else:
+            out=input
         out,_=self.gru(out)
         out= out[:,-1,:]
-        out=self.activation1(self.fc1(out))
-        out=self.activation2(self.fc2(out))
+        out=self.activation2(self.fc1(out))
+        #out=self.activation2(self.fc2(out))
         
         return out
 
 INPUT_DIM=dataset.vocab_size
-HID_DIM = 256
-NUM_LAYERS = 2
+HID_DIM = 128
+NUM_LAYERS = 4
 
-comp_model=simpleGRN(input_dim=INPUT_DIM,emb_hidden=HID_DIM*4,hidden_size=HID_DIM,num_layers=NUM_LAYERS).to('cuda')
+comp_model=simpleGRU(input_dim=INPUT_DIM,emb_hidden=HID_DIM,hidden_size=HID_DIM,num_layers=NUM_LAYERS).to('cuda')
 
 optimizer = torch.optim.Adam(comp_model.parameters())
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+#scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1,last_epoch=-1,verbose=True)
 criterion = nn.BCELoss()
-
-optimizer.state_dict().keys()
-optimizer.state_dict()['param_groups']
 
 N_EPOCHS = 100
 CLIP = 1
@@ -409,4 +286,49 @@ test_loss = evaluate_kor(comp_model, test_iter, criterion)
 print(f'| Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):7.3f} |')
 
 accuracy_score_kor(comp_model,train_iter,valid_iter,test_iter)
+
+for redata in train_iter:
+    break
+
+tdata=redata[0][3]
+tlabel=redata[1][3]
+
+
+
+baseline=torch.zeros(50).type(dtype=torch.long).to(device)
+input=tdata.to(device)
+
+ig=integrated_gradients(baseline,input,comp_model)
+
+ig2=torch.sum(ig,dim=1)
+
+def get_color(attr):
+    if attr > 0:
+        g = int(128*attr) + 127
+        b = 128 - int(64*attr)
+        r = 128 - int(64*attr)
+    else:
+        g = 128 + int(64*attr)
+        b = 128 + int(64*attr)
+        r = int(-128*attr) + 127
+    return r,g,b
+
+rgblist=[]
+
+for i in ig2:
+    rgblist.append(get_color(i))
+
+for i in tdata.numpy():
+    print(dataset.idx2word[str(i)])
+
+sentence=''
+for i,c in zip(tdata.numpy(),rgblist):
+    if i!=0:
+        word=dataset.idx2word[str(i)]
+        sentence=sentence+"\033[38;2;{};{};{}m {} \033[0m".format(c[0],c[1],c[2],word)
+
+print(sentence)
+
+
+
 
---
+++ Integrated_gradient/implement/ig_pytorch.py
@@ -0,0 +1,75 @@
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+import torchtext
+from torchtext.datasets import IMDB
+from tqdm import tqdm
+from NLP.transformers_module import *
+from copy import deepcopy
+from collections import defaultdict as ddict
+
+import matplotlib as mpl
+from konlpy.tag import Okt
+from torch.utils.data import TensorDataset, DataLoader,Dataset, random_split
+import json
+from tqdm import tqdm
+
+def interpolate_language(input,baseline,alphas,embedding_layer):
+    alphas_x = alphas.unsqueeze(1).unsqueeze(2)
+    baseline=embedding_layer(baseline)
+    baseline_x = torch.unsqueeze(baseline, dim=0)
+    
+    input_x=embedding_layer(input)
+    input_x=torch.unsqueeze(input_x,axis=0)
+    delta = input_x - baseline_x
+    sentence = baseline_x +  alphas_x * delta
+    return sentence
+
+def compute_gradients(sentence,model):
+    model.train()
+    model.zero_grad()
+    output=model(sentence).sum()
+    input_grad=torch.autograd.grad(output,sentence)[0]
+    
+    return input_grad
+
+def integral_approximation(gradients):
+    # riemann_trapezoidal
+    grads = (gradients[:-1] + gradients[1:]) / 2.0
+    integrated_gradients = torch.mean(grads, dim=0)
+    return integrated_gradients
+
+def integrated_gradients(baseline,input,model,m_steps=300,batch_size=32,device='cuda'):
+    # 0. Take Embedding Layer
+    embedding=model.embedding.to(device)
+    
+    # 1. Generate alphas
+    alphas = torch.linspace(start=0, end= 1,steps=m_steps).to(device)
+
+    # Accumulate gradients across batches
+    integrated_gradients = 0.0
+
+    # Batch alpha images
+    ds = DataLoader(alphas, batch_size=batch_size, shuffle=False, drop_last=False)
+    
+    for batch in ds:
+
+    # 2. Generate interpolated sentence
+        batch_interpolated_inputs = interpolate_language(input,baseline=baseline,alphas=batch,embedding_layer=embedding)
+
+    # 3. Compute gradients between model outputs and interpolated inputs
+        batch_gradients = compute_gradients(sentence=batch_interpolated_inputs,model=model)
+
+    # 4. Average integral approximation. Summing integrated gradients across batches.
+        integrated_gradients += integral_approximation(gradients=batch_gradients)
+
+    
+    # 5. Scale integrated gradients with respect to input
+    
+    scale=embedding(input)-embedding(baseline) 
+    scaled_integrated_gradients = scale * integrated_gradients
+    return scaled_integrated_gradients
